e6ef3af58c50eb2a6bb4f3c91b4b03fe126bd076
pallavisamudrala <pl.samudrala@gmail.com>
2017-07-21 16:52:13 -0400

    final files: qcache outputs for sub-03

--- sub-03/scripts/recon-all.cmd
+++ sub-03/scripts/recon-all.cmd
@@ -432,3 +432,8 @@ talairach_avi log file is transforms/talairach_avi.log...
 #--------------------------------------------
 #@# Qdec Cache preproc lh thickness fsaverage Fri Jul 21 16:23:20 EDT 2017
 \n mris_preproc --s sub-03 --hemi lh --meas thickness --target fsaverage --out lh.thickness.fsaverage.mgh \n
+\n\n#---------------------------------
+# New invocation of recon-all Fri Jul 21 16:52:08 EDT 2017 
+#--------------------------------------------
+#@# Qdec Cache preproc lh thickness fsaverage Fri Jul 21 16:52:10 EDT 2017
+\n mris_preproc --s sub-03 --hemi lh --meas thickness --target fsaverage --out lh.thickness.fsaverage.mgh \n
